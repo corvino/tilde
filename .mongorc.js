cace2788b45e4b204d8c534caee68122d255a3b3
Nathan Corvino <nathan@corvino.com>
2016-07-15 16:42:39 -0700

    Add simple-simple .mongorc.js.

---
+++ .mongorc.js
@@ -0,0 +1,15 @@
+EDITOR="emacs"
+
+prompt = function() {
+  if (typeof db == 'undefined') {
+    return '(nodb)> ';
+  }
+  // Check the last db operation
+  try {
+    db.runCommand({getLastError:1});
+  }
+  catch (e) {
+    print(e);
+  }
+  return db+"> ";
+};
